45fd370ac6d3d39fc1bfb6c77d7b9aae5133d720
emmaroche <20088680@mail.wit.ie>
2022-11-16 16:45:45 +0000

    Add code in the utils/persistence packages files

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging
+    implementation("io.github.microutils:kotlin-logging:3.0.0")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.1")
+    // https://mvnrepository.com/artifact/com.thoughtworks.xstream/xstream
+    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    // https://mvnrepository.com/artifact/org.codehaus.jettison/jettison
+    implementation("org.codehaus.jettison:jettison:1.4.1")
 }
 
 tasks.test {
