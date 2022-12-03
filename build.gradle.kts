93703b7f2292501d586bd296dd8270f286ad2454
emmaroche <20088680@mail.wit.ie>
2022-12-03 15:29:45 +0000

    Add dokka via gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.6.10"
     application
 }
 
@@ -15,13 +17,15 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging
-    implementation("io.github.microutils:kotlin-logging:3.0.0")
+    implementation("io.github.microutils:kotlin-logging:3.0.4")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
-    implementation("org.slf4j:slf4j-simple:2.0.1")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
     // https://mvnrepository.com/artifact/com.thoughtworks.xstream/xstream
-    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    implementation("com.thoughtworks.xstream:xstream:1.4.19")
     // https://mvnrepository.com/artifact/org.codehaus.jettison/jettison
-    implementation("org.codehaus.jettison:jettison:1.4.1")
+    implementation("org.codehaus.jettison:jettison:1.5.2")
+    // For generating a Dokka Site from KDoc
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
 }
 
 tasks.test {
